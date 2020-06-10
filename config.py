fb85a6546baa9ded9355dc661c28bb792a96916d
mohemmedalamry <mohemmedalamry@gmail.com>
2020-06-10 06:31:43 +0300

    test

--- config.py
+++ config.py
@@ -2,13 +2,16 @@ import os
 SECRET_KEY = os.urandom(32)
 # Grabs the folder where the script runs.
 basedir = os.path.abspath(os.path.dirname(__file__))
-
+dbUSER = os.environ.get('MD') 
+dbPASS = os.environ.get('Mp')
 # Enable debug mode.
 DEBUG = True
 
-# Connect to the database
-
+print(dbUSER)
+print(dbPASS)
+# Connect to the database 
+#postgres
 
 # TODO IMPLEMENT DATABASE URL
-SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:SL5078dm@localhost:5432/fyyurapp'
+SQLALCHEMY_DATABASE_URI = 'postgresql://{}:{}@localhost:5432/fyyurapp'.format(dbUSER,dbPASS)
 SQLALCHEMY_TRACK_MODIFICATIONS = False
